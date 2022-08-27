a65a51653ebab93fa8c7f5de375b042e862253c3
gabewin <gabewin@protonmail.com>
2022-08-27 13:36:14 -0700

    exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,6 +14,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good+bad+neutral
+  const average = (good-bad)/all
+  const positive = (good/all)*100
 
   const handleGood = () => {
     //setAll(allClicks.concat('L'))
@@ -41,6 +44,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>postive {positive} %</p>
     </div>
   )
 }

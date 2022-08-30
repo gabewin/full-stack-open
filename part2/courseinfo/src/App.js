926a314c2b4b881cacbeee004f0de0359fb040ea
gabewin <gabewin@protonmail.com>
2022-08-30 12:24:48 -0700

    lesson 2.1 exercises

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,71 @@
+const Course = ({course}) => 
+
+  <div key={course.id}>
+      <h1>{course.name}</h1>
+      {course.parts.map(
+        part => 
+        <p key={part.id}>{part.name} {part.exercises}</p>
+      )}
+       
+      <p>total of {course.parts.reduce((a,part) => a = a+part.exercises, 0)} exercises</p> 
+  </div>
+
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+    {courses.map(
+        course => 
+         <Course course={course} />
+      )}
+    </div>
+  )
+  
+}
+
+export default App

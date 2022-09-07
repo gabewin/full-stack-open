5b3cd54ca1d2ccf812561449c7b24b0437cec7dc
gabewin <gabewin@protonmail.com>
2022-09-07 06:30:09 -0700

    exercises 2.7 and 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,26 +3,45 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id:1, name: 'Arto Hellas' }
+    { id:1, name: 'Arto Hellas', number:'040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
-
-
+  const [newNumber, setNewNumber] = useState('')
+  
   const addPerson = (event) => {
     event.preventDefault()
+    console.log('submit', event)
 
-    
+    if (persons.some(obj => obj.name === newName)){
+        alert(`${newName} already exists in the phonebook`)
+
+    } else {
+
+
     const personObject = {
       id: persons.length+1,
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
+    }
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
+    console.log(event)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    console.log(event)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -34,6 +53,13 @@ const App = () => {
           <input 
             value={newName} 
             onChange={handleNameChange}
+         />
+        </div>
+        <div>
+          number: 
+          <input 
+            value={newNumber} 
+            onChange={handleNumberChange}
          />
         </div>
         <div>
